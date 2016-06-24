f55b0b529da8ef2ec3a162ecdeff30d0b25c788a
Simon <Simon@172.21.58.58>
2016-06-24 20:00:58 +0200

    UI hat nun alle Komponenten

--- Kinoticketverkauf_Vorlage_Blatt08/src/de/uni_hamburg/informatik/swt/se2/kino/werkzeuge/verkaufsfenster/VerkaufsFensterUI.java
+++ Kinoticketverkauf_Vorlage_Blatt08/src/de/uni_hamburg/informatik/swt/se2/kino/werkzeuge/verkaufsfenster/VerkaufsFensterUI.java
@@ -1,21 +1,49 @@
 package de.uni_hamburg.informatik.swt.se2.kino.werkzeuge.verkaufsfenster;
 
+import java.awt.BorderLayout;
 import java.awt.FlowLayout;
+import java.awt.GridLayout;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class VerkaufsFensterUI
 {
-    private JFrame _frame;
+    private JFrame _frame = new JFrame("VerkaufsFenster");;
+    private TastaturPanel _tastatur = new TastaturPanel();
+    private JPanel _linkeSeite = new JPanel();
+    private JPanel _verkaufsDialog = new JPanel();
+    private JPanel _bestaetigung = new JPanel();
+    private JLabel _preis = new JLabel("Preis: ");
+    private JTextField _erhaltenerBetrag = new JTextField(15);
+    private JLabel _rueckgeld = new JLabel("Rückgeld: ");
+    private JButton _ok = new JButton("OK");
+    private JButton _abbrechen = new JButton("Abbrechen");
     
     public VerkaufsFensterUI()
     {
-        _frame = new JFrame("VerkaufsFenster");
         _frame.setLayout(new FlowLayout());
-        _frame.add(new JPanel());
-        _frame.add(new TastaturPanel());
+        _frame.add(_linkeSeite);
+        
+        _linkeSeite.setLayout(new BorderLayout());
+        _linkeSeite.add(_verkaufsDialog, BorderLayout.CENTER);
+        _linkeSeite.add(_bestaetigung, BorderLayout.PAGE_END);
+        
+        _verkaufsDialog.setLayout(new GridLayout(3, 1));
+        _verkaufsDialog.add(_preis);
+        _verkaufsDialog.add(_erhaltenerBetrag);
+        _verkaufsDialog.add(_rueckgeld);
+        
+        _bestaetigung.setLayout(new FlowLayout());
+        _bestaetigung.add(_ok);
+        _bestaetigung.add(_abbrechen);
+        
+        _frame.add(_tastatur);
         _frame.setVisible(true);
-        _frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        _frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//nur für Testzwecke vorhanden (muss noch raus)
     }
 }
